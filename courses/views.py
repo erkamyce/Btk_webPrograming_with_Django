7fd48085263571efe2dbfe460364391fc5055660
Muhammed Erkam Yüce <115027797+erkamyce@users.noreply.github.com>
2023-11-06 15:02:21 +0300

    Initial commit

---
+++ courses/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home (request):
+    return HttpResponse('Anasayfa')
+def course (request):
+    return HttpResponse('Kurs Listeleri')
