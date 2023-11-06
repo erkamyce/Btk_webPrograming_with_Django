c424b57cf285d15b4dab5459c13298bc05fc7a1b
Muhammed Erkam Yüce <115027797+erkamyce@users.noreply.github.com>
2023-11-06 15:05:01 +0300

    deneme commit

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# deneme commit
 def home (request):
     return HttpResponse('Anasayfa')
 def course (request):
